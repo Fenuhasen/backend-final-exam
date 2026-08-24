cd414a31005a2e661d950bc0e7d800758a1ec800
Oundjatchi <suchiwa898@gmail.com>
2026-08-24 18:28:33 +0300

    Update course.ts

--- src/model/course.ts
+++ src/model/course.ts
@@ -1,1 +1,18 @@
+export interface Course {
+  id_course: number;
+  code: string;
+  name: string;
+  description: string;
+}
 
+export interface CreateCourseInput {
+  code: string;
+  name: string;
+  description?: string;
+}
+
+export interface UpdateCourseInput {
+  code?: string;
+  name?: string;
+  description?: string;
+}
