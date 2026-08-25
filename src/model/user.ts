189b2faa9fc98fdc3b423d3ee3b90df3204316af
Minosoa Fanantenana Andrianarivoharivony <hei.minosoa.3@gmail.com>
2026-08-25 10:50:54 +0300

    feat: merge 25080 to test (#4)
    * feat: implement student backend
    
    * feat: implement admin features
    
    * refactor: delete password returns
    
    * refactor: match model with spec

---
+++ src/model/user.ts
@@ -0,0 +1,63 @@
+export enum UserRole {
+  ADMIN = "ADMIN",
+  ETUDIANT = "ETUDIANT"
+}
+
+export enum UserStatus {
+  ACTIF = "ACTIF",
+  DESACTIVE = "DESACTIVE"
+}
+
+export interface User {
+  id_user: number;
+  first_name: string;
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  status: UserStatus;
+  created_at: Date;
+}
+
+export interface Student extends User {
+  role: UserRole.ETUDIANT;
+}
+
+export interface Admin extends User {
+  role: UserRole.ADMIN;
+}
+
+export interface UpdateStudentDTO {
+  firstName?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
+export interface CreateAdminDTO {
+  firstName: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface UpdateAdminDTO {
+  firstName?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
+export interface CreateStudentDTO {
+  firstName?: string; 
+  name: string;        
+  email: string;
+  password: string;
+}
+
+export interface UpdateStudentDTO {
+  firstName?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+}
